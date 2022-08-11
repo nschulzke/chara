a5cef1e21eceb06b1cf503a2d74231fd3e13af3d
Nathan Schulzke <nschulzke@gmail.com>
2022-08-11 10:19:17 -0600

    Initial setup of type checking

--- src/typechecker.rs
+++ src/typechecker.rs
@@ -4,21 +4,38 @@ use crate::parser::{Cycle, Factor, TypeAnnotation};
 
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub enum Type {
+    Param(usize),
     Int,
     Bool,
     String,
-    Function(Vec<Type>, Vec<Type>),
+    Unit,
+    Tuple(Vec<Type>),
+    Function(Box<Type>, Box<Type>),
+}
+
+impl Type {
+    fn function(arg: Type, ret: Type) -> Type {
+        Type::Function(Box::new(arg), Box::new(ret))
+    }
 }
 
 struct TypeChecker {
     environment: HashMap<String, Type>,
+    param_count: usize,
 }
 
 impl TypeChecker {
     fn new() -> Self {
         Self {
             environment: HashMap::new(),
+            param_count: 0,
         }
+    }
+
+    fn new_param(&mut self) -> Type {
+        let parameter_count = self.param_count;
+        self.param_count += 1;
+        Type::Param(parameter_count)
     }
 
     fn type_from_annotation(&self, annotation: &TypeAnnotation) -> Result<Type, Error> {
@@ -37,7 +54,7 @@ impl TypeChecker {
                 if in_type_errors.len() > 0 || out_type_errors.len() > 0 {
                     return Err(Error::TypeError("Error in function type".to_string(), token.clone(), ));
                 }
-                Ok(Type::Function(in_types, out_types))
+                Ok(Type::function(Type::Tuple(in_types), Type::Tuple(out_types)))
             },
             TypeAnnotation::Identifier(name, _) if name == "Int" => Ok(Type::Int),
             TypeAnnotation::Identifier(name, _) if name == "Bool" => Ok(Type::Bool),
@@ -46,61 +63,120 @@ impl TypeChecker {
         }
     }
 
-    fn check(&mut self, cycles: &Vec<Cycle>) -> Result<(), Error> {
+    pub fn check(&mut self, cycles: &Vec<Cycle>) -> Result<(), Error> {
         for cycle in cycles {
             self.check_cycle(cycle)?;
         }
         Ok(())
     }
 
-    fn check_cycle(&mut self, cycle: &Cycle) -> Result<(), Error> {
-        match cycle {
+    pub fn check_cycle(&mut self, cycle: &Cycle) -> Result<Type, Error> {
+        let t = match cycle {
             Cycle::Definition(name, annotation, factors) => {
-                self.check_definition(name, &self.type_from_annotation(annotation)?, factors)?;
+                self.check_definition(name, &self.type_from_annotation(annotation)?, factors)?
             }
             Cycle::Term(factors) => {
-                self.check_term(factors)?;
+                self.check_term(factors)?
+            }
+        };
+        Ok(t)
+    }
+
+    fn check_definition(&mut self, name: &str, annotation: &Type, factors: &Vec<Factor>) -> Result<Type, Error> {
+        self.environment.insert(name.to_string(), annotation.clone());
+        self.check_term(factors)
+    }
+
+    fn check_term(&mut self, factors: &Vec<Factor>) -> Result<Type, Error> {
+        let mut in_stack: Vec<Type> = Vec::new();
+        let mut out_stack: Vec<Type> = Vec::new();
+        for factor in factors {
+            let t = self.check_factor(factor)?;
+            match t {
+                Type::Param(_) => out_stack.push(t),
+                Type::Int => out_stack.push(t),
+                Type::Bool => out_stack.push(t),
+                Type::String => out_stack.push(t),
+                Type::Unit => { /* no-op */ }
+                Type::Tuple(ts) => out_stack.extend(ts),
+                Type::Function(t_in, t_out) => {
+                    let mut expected_in: Vec<Type> = Vec::new();
+                    match *t_in {
+                        Type::Param(_) => expected_in.push(*t_in),
+                        Type::Int => expected_in.push(*t_in),
+                        Type::Bool => expected_in.push(*t_in),
+                        Type::String => expected_in.push(*t_in),
+                        Type::Unit => { /* no-op */ }
+                        Type::Tuple(ts) => expected_in.extend(ts),
+                        Type::Function(_, _) => expected_in.push(*t_in),
+                    }
+                    for el in expected_in.into_iter().rev() {
+                        if out_stack.len() == 0 {
+                            in_stack.push(el);
+                        } else {
+                            out_stack.pop(); // TODO: Error if invalid value
+                        }
+                    }
+                    match *t_out {
+                        Type::Param(_) => out_stack.push(*t_out),
+                        Type::Int => out_stack.push(*t_out),
+                        Type::Bool => out_stack.push(*t_out),
+                        Type::String => out_stack.push(*t_out),
+                        Type::Unit => { /* no-op */ }
+                        Type::Tuple(ts) => out_stack.extend(ts),
+                        Type::Function(_, _) => out_stack.push(*t_out),
+                    }
+                }
             }
         }
-        Ok(())
+        Ok(Type::function(Type::Tuple(in_stack), Type::Tuple(out_stack)))
     }
 
-    fn check_definition(&mut self, name: &str, annotation: &Type, factors: &Vec<Factor>) -> Result<(), Error> {
-        self.environment.insert(name.to_string(), annotation.clone());
-        for factor in factors {
-            self.check_factor(factor)?;
-        }
-        Ok(())
-    }
-
-    fn check_term(&mut self, factors: &Vec<Factor>) -> Result<(), Error> {
-        for factor in factors {
-            self.check_factor(factor)?;
-        }
-        Ok(())
-    }
-
-    fn check_factor(&mut self, factor: &Factor) -> Result<(), Error> {
+    fn check_factor(&mut self, factor: &Factor) -> Result<Type, Error> {
         match factor {
-            Factor::Dup(_) => Ok(()),
-            Factor::Drop(_) => Ok(()),
-            Factor::Quote(_) => Ok(()),
-            Factor::Call(_) => Ok(()),
-            Factor::Cat(_) => Ok(()),
-            Factor::Swap(_) => Ok(()),
-            Factor::Ifte(_) => Ok(()),
-            Factor::Int(value, _) => Ok(()),
-            Factor::Bool(value, _) => Ok(()),
-            Factor::String(value, _) => Ok(()),
+            Factor::Dup(_) => {
+                let t = self.new_param();
+                Ok(Type::function(t.clone(), t))
+            },
+            Factor::Drop(_) => {
+                let t = self.new_param();
+                Ok(Type::function(t, Type::Unit))
+            },
+            Factor::Quote(_) => {
+                let t = self.new_param();
+                Ok(Type::function(t.clone(), Type::function(Type::Unit, t)))
+            },
+            Factor::Call(_) => {
+                unimplemented!()
+            },
+            Factor::Cat(_) => {
+                unimplemented!()
+            },
+            Factor::Swap(_) => {
+                let a = self.new_param();
+                let b = self.new_param();
+                Ok(Type::function(Type::function(a.clone(), b.clone()), Type::function(b, a)))
+            },
+            Factor::Ifte(_) => {
+                let t_in = self.new_param();
+                let t_out = self.new_param();
+                let t_condition = Type::function(t_in, Type::Bool);
+                let t_body = Type::function(Type::Bool, t_out.clone());
+                let t_input = Type::Tuple(vec![t_condition, t_body.clone(), t_body.clone()]);
+                let t_output = Type::Tuple(vec![t_out]);
+                Ok(Type::function(t_input, t_output))
+            },
+            Factor::Int(value, _) => Ok(Type::function(Type::Unit, Type::Int)),
+            Factor::Bool(value, _) => Ok(Type::function(Type::Unit, Type::Bool)),
+            Factor::String(value, _) => Ok(Type::function(Type::Unit, Type::String)),
             Factor::Identifier(name, token) => {
                 if !self.environment.contains_key(name) {
                     return Err(Error::TypeError(format!("Unknown identifier {}", name), token.clone()));
                 }
-                Ok(())
+                Ok(self.environment[name].clone())
             }
             Factor::Quotation(term) => {
-                self.check_term(term)?;
-                Ok(())
+                self.check_term(term)
             }
         }
     }
@@ -126,7 +202,7 @@ mod tests {
 
     #[test]
     fn allows_known_identifiers() {
-        let input = parse("[dup drop cat]").unwrap();
+        let input = parse("[dup drop dup]").unwrap();
         let mut typechecker = super::TypeChecker::new();
         typechecker.check(&input).unwrap();
     }
